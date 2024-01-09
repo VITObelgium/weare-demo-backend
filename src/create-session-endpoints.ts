89d3289aa966b5b0ebea3177682932fde6b7602c
vgemertb <beau.vangemert@vito.be>
2024-01-09 15:09:22 +0100

    Update Inrupt packages to fix redirect_uri mismatch error when logging in. Add try-catch so app won't break after exception at /login and /oidc-redirect. Add @weare/weare-libs repository (.npmrc).

--- src/create-session-endpoints.ts
+++ src/create-session-endpoints.ts
@@ -1,6 +1,6 @@
-import { getSessionFromStorage, Session as SolidSession } from "@inrupt/solid-client-authn-node";
-import { Express } from "express";
-import { NestedRedirectUrl } from "@weare/weare-libs";
+import {getSessionFromStorage, Session as SolidSession} from "@inrupt/solid-client-authn-node";
+import {Express} from "express";
+import {NestedRedirectUrl} from "@weare/weare-libs";
 
 export const CONFIG_QUERY_PARAM_OIDC_ISSUER_URL = "idp";
 export const CONFIG_BACKEND_TO_CHOOSE_OIDC_ISSUER = "BackendToChooseOidcIssuer";
@@ -21,46 +21,54 @@ function getRedirectUrlByQueryParameterOrDefault(
 export function createSessionEndpoints(app: Express) {
 
     app.get("/login", async (req, res, next) => {
-        const oidcRedirectUrl = `${process.env.PROTOCOL}://${process.env.HOST}${process.env.OIDC_REDIRECT_PATH}`;
+        try {
+            const oidcRedirectUrl = `${process.env.PROTOCOL}://${process.env.HOST}${process.env.OIDC_REDIRECT_PATH}`;
 
-        const session = new SolidSession();
-        console.log("Setting solidSessionId");
-        req.session!.solidSessionId = session.info.sessionId;
-        const redirectToSolidIdentityProvider = (url: string) => {
-            res.redirect(url);
-        };
+            const session = new SolidSession();
+            console.log("Setting solidSessionId");
+            req.session!.solidSessionId = session.info.sessionId;
+            const redirectToSolidIdentityProvider = (url: string) => {
+                res.redirect(url);
+            };
 
-        await session.login({
-            redirectUrl: oidcRedirectUrl,
-            oidcIssuer: process.env.OIDC_ISSUER_URL,
-            clientId: process.env.OIDC_CLIENT_ID,
-            handleRedirect: redirectToSolidIdentityProvider,
-        });
-
+            await session.login({
+                redirectUrl: oidcRedirectUrl,
+                oidcIssuer: process.env.OIDC_ISSUER_URL,
+                clientId: process.env.OIDC_CLIENT_ID,
+                handleRedirect: redirectToSolidIdentityProvider,
+            });
+        } catch (error) {
+            // A general error catcher which will, in turn, call the ExpressJS error handler.
+            next(error);
+        }
     });
 
     app.get("/oidc-redirect", async (req, res, next) => {
-        const session = await getSessionFromStorage(req.session!.solidSessionId);
-        const fullUrl = `${process.env.PROTOCOL}://${req.get("host")}${req.originalUrl}`;
+        try {
+            const session = await getSessionFromStorage(req.session!.solidSessionId);
+            const fullUrl = `${process.env.PROTOCOL}://${req.get("host")}${req.originalUrl}`;
 
-        await session!.handleIncomingRedirect(fullUrl);
+            await session!.handleIncomingRedirect(fullUrl);
 
-        if (session!.info.isLoggedIn) {
-            let redirectUrl;
-            try {
-                redirectUrl = getRedirectUrlByQueryParameterOrDefault(
-                    true
-                );
-            } catch (error) {
-                next(error);
-                return;
+            if (session!.info.isLoggedIn) {
+                let redirectUrl;
+                try {
+                    redirectUrl = getRedirectUrlByQueryParameterOrDefault(
+                        true
+                    );
+                } catch (error) {
+                    next(error);
+                    return;
+                }
+                res
+                    .status(302)
+                    .location(redirectUrl);
+                res.end();
             }
-            res
-            .status(302)
-            .location(redirectUrl);
-            res.end();
+        } catch (error) {
+            // A general error catcher which will, in turn, call the ExpressJS error handler.
+            next(error);
         }
-
     });
 
 }
